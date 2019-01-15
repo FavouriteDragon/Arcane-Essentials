f2eb1bb2a7e017bad05f8b7dadc605bbae217152
FavouriteDragon <ninjadragon008@icloud.com>
2019-01-14 20:46:18 -0800

    ok it should be working now

--- src/main/java/com/favouritedragon/arcaneessentials/common/spell/StormBlink.java
+++ src/main/java/com/favouritedragon/arcaneessentials/common/spell/StormBlink.java
@@ -35,67 +35,67 @@ public class StormBlink extends Spell {
 
 	@Override
 	public boolean cast(World world, EntityPlayer caster, EnumHand hand, int ticksInUse, SpellModifiers modifiers) {
-		float radius = 4 * modifiers.get(WizardryItems.range_upgrade);
+		float radius = 2 * modifiers.get(WizardryItems.range_upgrade);
 
 		if (!world.isRemote) {
-			RayTraceResult result = caster.rayTrace(80, 1);
+			RayTraceResult result = WizardryUtilities.rayTrace(80, world, caster, false);
 			if (result != null) {
 				BlockPos pos = result.getBlockPos();
-				caster.setPosition(pos.getX(), pos.getY(), pos.getZ());
-				WizardryUtilities.playSoundAtPlayer(caster, WizardrySounds.SPELL_LIGHTNING, SoundCategory.AMBIENT, 4.0F, 2.0F);
-				List<EntityLivingBase> targets = WizardryUtilities
-						.getEntitiesWithinRadius(radius, caster.posX, caster.posY, caster.posZ, world);
-				targets.remove(caster);
-				for (EntityLivingBase target : targets) {
-					if (MagicDamage.isEntityImmune(MagicDamage.DamageType.SHOCK, target)) {
-						caster.sendMessage(new TextComponentTranslation("spell.resist", target.getName(),
-								this.getNameForTranslationFormatted()));
-					} else {
-						if (target != caster) {
-							target.attackEntityFrom(
-									MagicDamage.causeDirectMagicDamage(caster, MagicDamage.DamageType.SHOCK),
-									4 * modifiers.get(WizardryItems.blast_upgrade));
-							double dx = target.posX - caster.posX;
-							double dz = target.posZ - caster.posZ;
-							// Normalises the velocity.
-							double vectorLength = MathHelper.sqrt(dx * dx + dz * dz);
-							dx /= vectorLength;
-							dz /= vectorLength;
+				if (caster.attemptTeleport(pos.getX(), pos.getY(), pos.getZ())) {
+					WizardryUtilities.playSoundAtPlayer(caster, WizardrySounds.SPELL_LIGHTNING, SoundCategory.AMBIENT, 4.0F, 2.0F);
+					List<EntityLivingBase> targets = WizardryUtilities
+							.getEntitiesWithinRadius(radius, caster.posX, caster.posY, caster.posZ, world);
+					targets.remove(caster);
+					for (EntityLivingBase target : targets) {
+						if (MagicDamage.isEntityImmune(MagicDamage.DamageType.SHOCK, target)) {
+							caster.sendMessage(new TextComponentTranslation("spell.resist", target.getName(),
+									this.getNameForTranslationFormatted()));
+						} else {
+							if (target != caster) {
+								target.attackEntityFrom(
+										MagicDamage.causeDirectMagicDamage(caster, MagicDamage.DamageType.SHOCK),
+										4 * modifiers.get(WizardryItems.blast_upgrade));
+								double dx = target.posX - caster.posX;
+								double dz = target.posZ - caster.posZ;
+								// Normalises the velocity.
+								double vectorLength = MathHelper.sqrt(dx * dx + dz * dz);
+								dx /= vectorLength;
+								dz /= vectorLength;
 
-							target.motionX = 2 * dx;
-							target.motionY = 0.2;
-							target.motionZ = 2 * dz;
+								target.motionX = 2 * dx;
+								target.motionY = 0.2;
+								target.motionZ = 2 * dz;
 
-							// Player motion is handled on that player's client so needs packets
-							if (target instanceof EntityPlayerMP) {
-								((EntityPlayerMP) target).connection.sendPacket(new SPacketEntityVelocity(target));
+								// Player motion is handled on that player's client so needs packets
+								if (target instanceof EntityPlayerMP) {
+									((EntityPlayerMP) target).connection.sendPacket(new SPacketEntityVelocity(target));
+								}
 							}
-						}
 
+
+						}
+					}
+					double x, y, z;
+					for (double theta = 0; theta <= 180; theta += 1) {
+						double dphi = 15 / Math.sin(Math.toRadians(theta));
+
+						for (double phi = 0; phi < 360; phi += dphi) {
+							double rphi = Math.toRadians(phi);
+							double rtheta = Math.toRadians(theta);
+
+							x = radius * Math.cos(rphi) * Math.sin(rtheta);
+							y = radius * Math.sin(rphi) * Math.sin(rtheta);
+							z = radius * Math.cos(rtheta);
+
+							Wizardry.proxy.spawnParticle(WizardryParticleType.SPARK, world, x + caster.posX, y + caster.posY, z + caster.posZ, 0, 0, 0, 10);
+						}
 
 					}
 				}
+				return true;
 			}
-			return true;
 		}
-		double x, y, z;
-		for (double theta = 0; theta <= 180; theta += 1) {
-			double dphi = 15 / Math.sin(Math.toRadians(theta));
 
-			for (double phi = 0; phi < 360; phi += dphi) {
-				double rphi = Math.toRadians(phi);
-				double rtheta = Math.toRadians(theta);
-
-				x = radius * Math.cos(rphi) * Math.sin(rtheta);
-				y = radius * 1 * Math.sin(rphi) * Math.sin(rtheta);
-				z = radius * 1 * Math.cos(rtheta);
-
-				if (world.isRemote) {
-					Wizardry.proxy.spawnParticle(WizardryParticleType.SPARK, world, x + caster.posX, y + caster.posY, z + caster.posZ, 0, 0, 0, 10);
-				}
-			}
-
-		}
 		return false;
 	}
 
@@ -104,63 +104,62 @@ public class StormBlink extends Spell {
 		float radius = 4 * modifiers.get(WizardryItems.range_upgrade);
 
 		if (!world.isRemote) {
-			RayTraceResult result = caster.rayTrace(80, 1);
+			RayTraceResult result = WizardryUtilities.rayTrace(80, world, caster, false);
 			if (result != null) {
 				BlockPos pos = result.getBlockPos();
-				caster.setPosition(pos.getX(), pos.getY(), pos.getZ());
-				world.playSound(caster.posX, caster.posY, caster.posZ, WizardrySounds.SPELL_LIGHTNING, SoundCategory.AMBIENT, 4.0F, 2.0F, true);
-				List<EntityLivingBase> targets = WizardryUtilities
-						.getEntitiesWithinRadius(radius, caster.posX, caster.posY, caster.posZ, world);
-				targets.remove(caster);
-				for (EntityLivingBase target1 : targets) {
-					if (MagicDamage.isEntityImmune(MagicDamage.DamageType.SHOCK, target1)) {
-						caster.sendMessage(new TextComponentTranslation("spell.resist", target1.getName(),
-								this.getNameForTranslationFormatted()));
-					} else {
-						if (target1 != caster) {
-							target1.attackEntityFrom(
-									MagicDamage.causeDirectMagicDamage(caster, MagicDamage.DamageType.SHOCK),
-									4 * modifiers.get(WizardryItems.blast_upgrade));
-							double dx = target1.posX - caster.posX;
-							double dz = target1.posZ - caster.posZ;
-							// Normalises the velocity.
-							double vectorLength = MathHelper.sqrt(dx * dx + dz * dz);
-							dx /= vectorLength;
-							dz /= vectorLength;
+				if (caster.attemptTeleport(pos.getX(), pos.getY(), pos.getZ())) {
+					world.playSound(caster.posX, caster.posY, caster.posZ, WizardrySounds.SPELL_LIGHTNING, SoundCategory.AMBIENT, 4.0F, 2.0F, true);
+					List<EntityLivingBase> targets = WizardryUtilities
+							.getEntitiesWithinRadius(radius, caster.posX, caster.posY, caster.posZ, world);
+					targets.remove(caster);
+					for (EntityLivingBase target1 : targets) {
+						if (MagicDamage.isEntityImmune(MagicDamage.DamageType.SHOCK, target1)) {
+							caster.sendMessage(new TextComponentTranslation("spell.resist", target1.getName(),
+									this.getNameForTranslationFormatted()));
+						} else {
+							if (target1 != caster) {
+								target1.attackEntityFrom(
+										MagicDamage.causeDirectMagicDamage(caster, MagicDamage.DamageType.SHOCK),
+										4 * modifiers.get(WizardryItems.blast_upgrade));
+								double dx = target1.posX - caster.posX;
+								double dz = target1.posZ - caster.posZ;
+								// Normalises the velocity.
+								double vectorLength = MathHelper.sqrt(dx * dx + dz * dz);
+								dx /= vectorLength;
+								dz /= vectorLength;
 
-							target1.motionX = 2 * dx;
-							target1.motionY = 0.2;
-							target1.motionZ = 2 * dz;
+								target1.motionX = 2 * dx;
+								target1.motionY = 0.2;
+								target1.motionZ = 2 * dz;
 
-							// Player motion is handled on that player's client so needs packets
-							if (target instanceof EntityPlayerMP) {
-								((EntityPlayerMP) target).connection.sendPacket(new SPacketEntityVelocity(target));
+								// Player motion is handled on that player's client so needs packets
+								if (target instanceof EntityPlayerMP) {
+									((EntityPlayerMP) target).connection.sendPacket(new SPacketEntityVelocity(target));
+								}
 							}
-						}
 
+
+						}
+					}
+					double x, y, z;
+					for (double theta = 0; theta <= 180; theta += 1) {
+						double dphi = 15 / Math.sin(Math.toRadians(theta));
+
+						for (double phi = 0; phi < 360; phi += dphi) {
+							double rphi = Math.toRadians(phi);
+							double rtheta = Math.toRadians(theta);
+
+							x = radius * Math.cos(rphi) * Math.sin(rtheta);
+							y = radius * Math.sin(rphi) * Math.sin(rtheta);
+							z = radius * Math.cos(rtheta);
+
+							Wizardry.proxy.spawnParticle(WizardryParticleType.SPARK, world, x + caster.posX, y + caster.posY, z + caster.posZ, 0, 0, 0, 10);
+						}
 
 					}
 				}
+				return true;
 			}
-			return true;
-		}
-		double x, y, z;
-		for (double theta = 0; theta <= 180; theta += 1) {
-			double dphi = 15 / Math.sin(Math.toRadians(theta));
-
-			for (double phi = 0; phi < 360; phi += dphi) {
-				double rphi = Math.toRadians(phi);
-				double rtheta = Math.toRadians(theta);
-
-				x = radius * Math.cos(rphi) * Math.sin(rtheta);
-				y = radius * 1 * Math.sin(rphi) * Math.sin(rtheta);
-				z = radius * 1 * Math.cos(rtheta);
-
-				if (world.isRemote) {
-					Wizardry.proxy.spawnParticle(WizardryParticleType.SPARK, world, x + caster.posX, y + caster.posY, z + caster.posZ, 0, 0, 0, 10);
-				}
-			}
-
 		}
 		return false;
 	}
