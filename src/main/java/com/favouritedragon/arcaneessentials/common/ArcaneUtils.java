0320f955f884bb6842fafcab215e2f028f030faf
FavouriteDragon <ninjadragon008@icloud.com>
2019-01-24 11:47:52 -0800

    methods ofr particle spawning- I can be lazier in the future

--- src/main/java/com/favouritedragon/arcaneessentials/common/ArcaneUtils.java
+++ src/main/java/com/favouritedragon/arcaneessentials/common/ArcaneUtils.java
@@ -1,6 +1,11 @@
 package com.favouritedragon.arcaneessentials.common;
 
+import electroblob.wizardry.Wizardry;
+import electroblob.wizardry.util.WizardryParticleType;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 
 public class ArcaneUtils {
 	public static Vec3d rotateAroundAxisX(Vec3d v, double angle) {
@@ -32,5 +37,31 @@ public class ArcaneUtils {
 		x = v.x * cos - v.y * sin;
 		y = v.x * sin + v.y * cos;
 		return new Vec3d(x, y, v.z);
+	}
+
+	public static void spawnDirectionalVortex(World world, EntityLivingBase entity, Vec3d direction, int particleAmount, double vortexLength, double radiusScale, EnumParticleTypes particle, double posX, double posY, double posZ,
+									   double particleSpeed) {
+
+	}
+
+	public static void spawnDirectionalVortex(World world, EntityLivingBase entity, Vec3d direction, int particleAmount, double vortexLength, double radiusScale, WizardryParticleType particle, double posX, double posY, double posZ,
+									   double velX, double velY, double velZ, int maxAge) { ;
+		for (int angle = 0; angle < particleAmount; angle++) {
+			double radius = angle / radiusScale;
+			double x = radius * Math.cos(angle);
+			double y = angle / (180/vortexLength);
+			double z = radius * Math.sin(angle);
+			Vec3d pos = new Vec3d(x, y, z);
+			if (entity != null && direction != null) {
+				pos = ArcaneUtils.rotateAroundAxisX(pos, entity.rotationPitch + 90);
+				pos = ArcaneUtils.rotateAroundAxisY(pos, entity.rotationYaw);
+				Wizardry.proxy.spawnParticle(particle, world, pos.x + posX + direction.x, pos.y + posY + direction.y,
+						pos.z + posZ + direction.z, velX, velY, velZ, maxAge);
+			}
+			else {
+				Wizardry.proxy.spawnParticle(particle, world, x + posX, y + posY,
+						z + posZ, velX, velY, velZ, maxAge);
+			}
+		}
 	}
 }
--- src/main/java/com/favouritedragon/arcaneessentials/common/spell/OceanBurst.java
+++ src/main/java/com/favouritedragon/arcaneessentials/common/spell/OceanBurst.java
@@ -2,7 +2,6 @@ package com.favouritedragon.arcaneessentials.common.spell;
 
 import com.favouritedragon.arcaneessentials.ArcaneEssentials;
 import com.favouritedragon.arcaneessentials.common.ArcaneUtils;
-import electroblob.wizardry.Wizardry;
 import electroblob.wizardry.constants.Element;
 import electroblob.wizardry.constants.SpellType;
 import electroblob.wizardry.constants.Tier;
@@ -32,17 +31,21 @@ public class OceanBurst extends Spell {
 		}
 		//Spawn particles
 		Vec3d look = caster.getLookVec();
-		for (int angle = 0; angle < 180; angle++) {
-			double radius = angle / 120F;
-			double x = radius * Math.cos(angle);
-			double y = angle / (180/range);
-			double z = radius * Math.sin(angle);
-			Vec3d pos = new Vec3d(x, y, z);
-			pos = ArcaneUtils.rotateAroundAxisX(pos, caster.rotationPitch + 90);
-			pos = ArcaneUtils.rotateAroundAxisY(pos, caster.rotationYaw);
-			Wizardry.proxy.spawnParticle(WizardryParticleType.MAGIC_BUBBLE, world, pos.x + caster.posX + look.x, pos.y + caster.posY + caster.getEyeHeight() + look.y,
-					pos.z + caster.posZ + look.z, 0, 0, 0, 10);
-		}
+		double random = world.rand.nextDouble() / 10;
+		ArcaneUtils.spawnDirectionalVortex(world, caster, look, 240, range, 240 / 1.5, WizardryParticleType.MAGIC_BUBBLE, caster.posX, caster.posY,
+				caster.posZ, random, random, random, 10);
+		/*	for (int angle = 0; angle < 180; angle++) {
+				double radius = angle / 120F;
+				double x = radius * Math.cos(angle);
+				double y = angle / (180 / range);
+				double z = radius * Math.sin(angle);
+				Vec3d pos = new Vec3d(x, y, z);
+				pos = ArcaneUtils.rotateAroundAxisX(pos, caster.rotationPitch + 90);
+				pos = ArcaneUtils.rotateAroundAxisY(pos, caster.rotationYaw);
+				Wizardry.proxy.spawnParticle(WizardryParticleType.MAGIC_BUBBLE, world, pos.x + caster.posX + look.x, pos.y + caster.posY + caster.getEyeHeight() + look.y,
+						pos.z + caster.posZ + look.z, 0, 0, 0, 10);
+			}**/
+
 		return false;
 	}
 
@@ -55,7 +58,7 @@ public class OceanBurst extends Spell {
 		//Spawn particles
 		for (int angle = 0; angle < 180; angle++) {
 			double x = Math.cos(angle);
-			double y = 180F/angle;
+			double y = 180F / angle;
 			double z = Math.sin(angle);
 		}
 
