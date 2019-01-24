3c1aff6d90054fc30b634946ec565ed4a3da6c39
FavouriteDragon <ninjadragon008@icloud.com>
2019-01-24 13:08:05 -0800

    adjusted range and knockback

--- src/main/java/com/favouritedragon/arcaneessentials/common/spell/OceanBurst.java
+++ src/main/java/com/favouritedragon/arcaneessentials/common/spell/OceanBurst.java
@@ -37,7 +37,7 @@ public class OceanBurst extends Spell {
 		//Spawn particles
 		ArcaneUtils.spawnDirectionalVortex(world, caster, look.scale(0.8), 240, range, 240 / 1.5, WizardryParticleType.MAGIC_BUBBLE, caster.posX, caster.posY + 1.2,
 				caster.posZ, 0, 0, 0, 8);
-		look = look.scale(range);
+		look = look.scale(range * 1.1);
 		if (!world.isRemote) {
 			AxisAlignedBB hitBox = new AxisAlignedBB(caster.posX, caster.posY + 1.2, caster.posZ, caster.posX + look.x, caster.posY + 1.2 + look.y, caster.posZ
 			+ look.z);
@@ -46,7 +46,7 @@ public class OceanBurst extends Spell {
 				for (EntityLivingBase e : hit) {
 					if (e != caster)  {
 						e.attackEntityFrom(MagicDamage.causeDirectMagicDamage(caster, MagicDamage.DamageType.MAGIC),4 + 2 * modifiers.get(WizardryItems.blast_upgrade));
-						Vec3d knockback = caster.getLookVec().scale(range);
+						Vec3d knockback = caster.getLookVec().scale(range/2);
 						e.motionX += knockback.x;
 						e.motionY += knockback.y + 0.2;
 						e.motionZ += knockback.z;
