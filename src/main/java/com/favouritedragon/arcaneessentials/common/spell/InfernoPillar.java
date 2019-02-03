4403da56ea440958c6884bee67dc4fe86fb84e54
FavouriteDragon <ninjadragon008@icloud.com>
2019-02-03 11:38:06 -0800

    Mostly sure I've fixed the issue- it doesn't appear to happen anymore

--- src/main/java/com/favouritedragon/arcaneessentials/common/spell/InfernoPillar.java
+++ src/main/java/com/favouritedragon/arcaneessentials/common/spell/InfernoPillar.java
@@ -21,7 +21,7 @@ import net.minecraft.world.World;
 public class InfernoPillar extends Spell {
 
 	public InfernoPillar() {
-		super(Tier.ADVANCED, 60, Element.FIRE, "inferno_pillar", SpellType.DEFENCE, 200, EnumAction.BOW, false, ArcaneEssentials.MODID);
+		super(Tier.ADVANCED, 60, Element.FIRE, "inferno_pillar", SpellType.DEFENCE, 180, EnumAction.BOW, false, ArcaneEssentials.MODID);
 	}
 
 	@Override
