9ef2f9edba2dcefa2f9828d39f87f3979a212e4f
FavouriteDragon <ninjadragon008@icloud.com>
2019-07-25 18:17:51 -0700

    balancing ocean burst

--- src/main/java/com/favouritedragon/arcaneessentials/common/spell/water/OceanBurst.java
+++ src/main/java/com/favouritedragon/arcaneessentials/common/spell/water/OceanBurst.java
@@ -38,34 +38,34 @@ public class OceanBurst extends Spell {
 		if (world.isRemote) {
 			//Spawn particles
 				for(int i = 0; i < 40; i++) {
-					double x1 = caster.posX + look.x + world.rand.nextFloat() / 3 - 0.1f;
-					double y1 = WizardryUtilities.getPlayerEyesPos(caster) - 0.4f + world.rand.nextFloat() / 5 - 0.1f;
-					double z1 = caster.posZ + look.z + world.rand.nextFloat() / 3 - 0.1f;
+					double x1 = caster.posX + look.x + world.rand.nextFloat() / 10 - 0.05f;
+					double y1 = WizardryUtilities.getPlayerEyesPos(caster) - 0.4f + world.rand.nextFloat() / 10 - 0.05f;
+					double z1 = caster.posZ + look.z + world.rand.nextFloat() / 10 - 0.05f;
 
 					//Using the random function each time ensures a different number for every value, making the ability "feel" better.
 					Wizardry.proxy.spawnParticle(WizardryParticleType.MAGIC_BUBBLE, world, x1, y1, z1,
 							look.x * mult * ArcaneUtils.getRandomNumberInRange(1, 100) / 25
-									+ ArcaneUtils.getRandomNumberInRange(-10, 10) / 10F,
+									+ ArcaneUtils.getRandomNumberInRange(-10, 10) / 20F,
 							look.y * mult * ArcaneUtils.getRandomNumberInRange(1, 100) / 25
-									+ ArcaneUtils.getRandomNumberInRange(-10, 10) / 10F,
+									+ ArcaneUtils.getRandomNumberInRange(-10, 10) / 20F,
 							look.z * mult * ArcaneUtils.getRandomNumberInRange(1, 100) / 25
-									+ ArcaneUtils.getRandomNumberInRange(-10, 10) / 10F,
+									+ ArcaneUtils.getRandomNumberInRange(-10, 10) / 20F,
 							0);
 					Wizardry.proxy.spawnParticle(WizardryParticleType.MAGIC_BUBBLE, world, x1, y1, z1,
 							look.x * mult * ArcaneUtils.getRandomNumberInRange(1, 100) / 25
-									+ ArcaneUtils.getRandomNumberInRange(-10, 10) / 10F,
+									+ ArcaneUtils.getRandomNumberInRange(-10, 10) / 20F,
 							look.y * mult * ArcaneUtils.getRandomNumberInRange(1, 100) / 25
-									+ ArcaneUtils.getRandomNumberInRange(-10, 10) / 10F,
+									+ ArcaneUtils.getRandomNumberInRange(-10, 10) / 20F,
 							look.z * mult * ArcaneUtils.getRandomNumberInRange(1, 100) / 25
-									+ ArcaneUtils.getRandomNumberInRange(-10, 10) / 10F,
+									+ ArcaneUtils.getRandomNumberInRange(-10, 10) / 20F,
 							0);
 				}
 		}
 		if (!world.isRemote) {
 			Vec3d startPos = new Vec3d(caster.posX, WizardryUtilities.getPlayerEyesPos(caster), caster.posZ);
-			Vec3d endPos = startPos.add(caster.getLookVec().scale(mult * 4 * range));
-			ArcaneUtils.vortexEntityCollision(world, caster, null, startPos, endPos, 0.74F, 4 + 2 * modifiers.get(WizardryItems.blast_upgrade),
-					look.scale(2 + 1 * modifiers.get(WizardryItems.blast_upgrade)), SPLASH, true);
+			Vec3d endPos = startPos.add(caster.getLookVec().scale(mult * range));
+			ArcaneUtils.vortexEntityCollision(world, caster, null, startPos, endPos, 0.74F, 3 + 2 * modifiers.get(WizardryItems.blast_upgrade),
+					look.scale(1 + 1 * modifiers.get(WizardryItems.blast_upgrade)), SPLASH, true);
 			WizardryUtilities.playSoundAtPlayer(caster, SoundEvents.ENTITY_GENERIC_SWIM, 2.0F,
 					world.rand.nextFloat() * 0.2F + 1.0F);
 			WizardryUtilities.playSoundAtPlayer(caster, WizardrySounds.SPELL_ICE, 1F,
@@ -87,32 +87,32 @@ public class OceanBurst extends Spell {
 		if (world.isRemote) {
 			//Spawn particles
 			for(int i = 0; i < 40; i++) {
-				double x1 = caster.posX + look.x + world.rand.nextFloat() / 3 - 0.1f;
-				double y1 = caster.getEyeHeight() + caster.getEntityBoundingBox().minY - 0.4f + world.rand.nextFloat() / 5 - 0.1f;
-				double z1 = caster.posZ + look.z + world.rand.nextFloat() / 3 - 0.1f;
+				double x1 = caster.posX + look.x + world.rand.nextFloat() / 10 - 0.05f;
+				double y1 = caster.getEyeHeight() + caster.getEntityBoundingBox().minY - 0.4f + world.rand.nextFloat() / 10 - 0.05f;
+				double z1 = caster.posZ + look.z + world.rand.nextFloat() / 10 - 0.05f;
 
 				//Using the random function each time ensures a different number for every value, making the ability "feel" better.
 				Wizardry.proxy.spawnParticle(WizardryParticleType.MAGIC_BUBBLE, world, x1, y1, z1,
-						look.x * mult * ArcaneUtils.getRandomNumberInRange(1, 100) / 25
-								+ ArcaneUtils.getRandomNumberInRange(-10, 10) / 10F,
-						look.y * mult * ArcaneUtils.getRandomNumberInRange(1, 100) / 25
-								+ ArcaneUtils.getRandomNumberInRange(-10, 10) / 10F,
-						look.z * mult * ArcaneUtils.getRandomNumberInRange(1, 100) / 25
-								+ ArcaneUtils.getRandomNumberInRange(-10, 10) / 10F,
+						look.x * mult * ArcaneUtils.getRandomNumberInRange(1, 100) / 50
+								+ ArcaneUtils.getRandomNumberInRange(-10, 10) / 20F,
+						look.y * mult * ArcaneUtils.getRandomNumberInRange(1, 100) / 50
+								+ ArcaneUtils.getRandomNumberInRange(-10, 10) / 20F,
+						look.z * mult * ArcaneUtils.getRandomNumberInRange(1, 100) / 50
+								+ ArcaneUtils.getRandomNumberInRange(-10, 10) / 20F,
 						0);
 				Wizardry.proxy.spawnParticle(WizardryParticleType.MAGIC_BUBBLE, world, x1, y1, z1,
-						look.x * mult * ArcaneUtils.getRandomNumberInRange(1, 100) / 25
-								+ ArcaneUtils.getRandomNumberInRange(-10, 10) / 10F,
-						look.y * mult * ArcaneUtils.getRandomNumberInRange(1, 100) / 25
-								+ ArcaneUtils.getRandomNumberInRange(-10, 10) / 10F,
-						look.z * mult * ArcaneUtils.getRandomNumberInRange(1, 100) / 25
-								+ ArcaneUtils.getRandomNumberInRange(-10, 10) / 10F,
+						look.x * mult * ArcaneUtils.getRandomNumberInRange(1, 100) / 50
+								+ ArcaneUtils.getRandomNumberInRange(-10, 10) / 20F,
+						look.y * mult * ArcaneUtils.getRandomNumberInRange(1, 100) / 50
+								+ ArcaneUtils.getRandomNumberInRange(-10, 10) / 20F,
+						look.z * mult * ArcaneUtils.getRandomNumberInRange(1, 100) / 50
+								+ ArcaneUtils.getRandomNumberInRange(-10, 10) / 20F,
 						0);
 			}
 		}
 		if (!world.isRemote) {
 			Vec3d startPos = new Vec3d(caster.posX, caster.getEyeHeight() + caster.getEntityBoundingBox().minY, caster.posZ);
-			Vec3d endPos = startPos.add(caster.getLookVec().scale(mult * 4 * range));
+			Vec3d endPos = startPos.add(caster.getLookVec().scale(mult * range));
 			ArcaneUtils.vortexEntityCollision(world, caster, null, startPos, endPos, 0.74F, 4 + 2 * modifiers.get(WizardryItems.blast_upgrade),
 					look.scale(2 + 1 * modifiers.get(WizardryItems.blast_upgrade)), SPLASH, true);
 			world.playSound(caster.posX, caster.posY, caster.posZ, SoundEvents.ENTITY_GENERIC_SWIM, SoundCategory.HOSTILE, 2.0F,
