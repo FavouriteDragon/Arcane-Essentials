ab76f2de7df407e9f6c67781e2e4298aacfb14fb
FavouriteDragon <ninjadragon008@icloud.com>
2019-07-25 18:20:57 -0700

    spellz

---
+++ src/main/java/com/favouritedragon/arcaneessentials/common/spell/storm/ThunderingChain.java
@@ -0,0 +1,23 @@
+package com.favouritedragon.arcaneessentials.common.spell.storm;
+
+import electroblob.wizardry.constants.Element;
+import electroblob.wizardry.constants.SpellType;
+import electroblob.wizardry.constants.Tier;
+import electroblob.wizardry.spell.Spell;
+import electroblob.wizardry.util.SpellModifiers;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumAction;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+
+public class ThunderingChain extends Spell {
+
+	public ThunderingChain(Tier tier, int cost, Element element, String name, SpellType type, int cooldown, EnumAction action, boolean isContinuous) {
+		super(tier, cost, element, name, type, cooldown, action, isContinuous);
+	}
+
+	@Override
+	public boolean cast(World world, EntityPlayer caster, EnumHand hand, int ticksInUse, SpellModifiers modifiers) {
+		return false;
+	}
+}
