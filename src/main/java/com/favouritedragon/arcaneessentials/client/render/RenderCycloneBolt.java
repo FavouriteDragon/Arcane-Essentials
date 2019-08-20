1e0e1006ea6ba39ea7d01825527721c32a9b31fb
FavouriteDragon <ninjadragon008@icloud.com>
2019-08-19 18:05:01 -0700

    cyclone bolt is now super dope!

--- src/main/java/com/favouritedragon/arcaneessentials/client/render/RenderCycloneBolt.java
+++ src/main/java/com/favouritedragon/arcaneessentials/client/render/RenderCycloneBolt.java
@@ -3,33 +3,82 @@ package com.favouritedragon.arcaneessentials.client.render;
 import com.favouritedragon.arcaneessentials.common.entity.EntityCycloneBolt;
 import com.favouritedragon.arcaneessentials.common.util.ArcaneUtils;
 import electroblob.wizardry.util.ParticleBuilder;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+import static com.favouritedragon.arcaneessentials.client.render.RenderUtils.drawSphere;
+
 public class RenderCycloneBolt extends Render<EntityCycloneBolt> {
+
+	private static final float EXPANSION_TIME = 3;
 
 	public RenderCycloneBolt(RenderManager renderManager) {
 		super(renderManager);
 	}
 
+	//Copied from the forcefiled class
 	@Override
-	public void doRender(EntityCycloneBolt entity, double x, double y, double z, float entityYaw, float partialTicks) {
+	public void doRender(@Nonnull EntityCycloneBolt entity, double x, double y, double z, float entityYaw, float partialTicks) {
 		super.doRender(entity, x, y, z, entityYaw, partialTicks);
+		GlStateManager.pushMatrix();
+
+		GlStateManager.disableLighting();
+		GlStateManager.enableBlend();
+		GlStateManager.disableTexture2D();
+		GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+		OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240f, 240f);
+
+		GlStateManager.translate(x, y, z);
+
+		float latStep = (float) Math.PI / 20;
+		float longStep = (float) Math.PI / 20;
+
+		float pulse = MathHelper.sin((entity.ticksExisted + partialTicks) / 10f);
+
+		float r = 1, g = 0.95F + 0.05f * pulse, b = 1;
+
+		float radius = entity.width / 2;
+		float a = 0.5f;
+
+		if (entity.ticksExisted > entity.getLifetime() - EXPANSION_TIME) {
+			radius *= 1 + 0.2f * (entity.ticksExisted + partialTicks - (entity.getLifetime() - EXPANSION_TIME)) / EXPANSION_TIME;
+			a *= Math.max(0, 1 - (entity.ticksExisted + partialTicks - (entity.getLifetime() - EXPANSION_TIME)) / EXPANSION_TIME);
+		} else if (entity.ticksExisted < EXPANSION_TIME) {
+			radius *= 1 - (EXPANSION_TIME - entity.ticksExisted - partialTicks) / EXPANSION_TIME;
+			a *= 1 - (EXPANSION_TIME - entity.ticksExisted - partialTicks) / EXPANSION_TIME;
+		}
+
+		// Draw the inside first
+		drawSphere(radius - 0.1f - 0.025f * pulse, latStep, longStep, true, r, g, b, a * 0.8F);
+		drawSphere(radius - 0.1f - 0.025f * pulse, latStep, longStep, false, 1, 1, 1, a * 0.8F);
+		drawSphere(radius, latStep, longStep, false, r, g, b, 0.7f * a);
+
+		//Particles
+		ParticleBuilder.create(ParticleBuilder.Type.BUFF).entity(entity).time(2).clr(1F, 1F, 1F).spawn(entity.world);
 		ArcaneUtils.spawnSpinningDirectionalVortex(entity.world, entity.getCaster(), Vec3d.ZERO, 12, 1, 0, 72,
 				EnumParticleTypes.EXPLOSION_NORMAL, entity.getPositionVector(), new Vec3d(0.4, 0.1, 0.4), new Vec3d(entity.motionX, entity.motionY, entity.motionZ));
-		ParticleBuilder.create(ParticleBuilder.Type.SPHERE).clr(1F, 1F, 1F).entity(entity)
-				.time(entity.getLifetime()).scale(3).spawn(entity.world);
-		ParticleBuilder.create(ParticleBuilder.Type.BUFF).entity(entity).time(2).clr(1F, 1F, 1F).spawn(entity.world);
+
+
+		GlStateManager.enableTexture2D();
+		GlStateManager.enableLighting();
+		GlStateManager.disableBlend();
+
+		GlStateManager.popMatrix();
+
 	}
 
 	@Nullable
 	@Override
-	protected ResourceLocation getEntityTexture(EntityCycloneBolt entity) {
+	protected ResourceLocation getEntityTexture(@Nonnull EntityCycloneBolt entity) {
 		return null;
 	}
 }
--- src/main/java/com/favouritedragon/arcaneessentials/client/render/RenderUtils.java
+++ src/main/java/com/favouritedragon/arcaneessentials/client/render/RenderUtils.java
@@ -2,6 +2,7 @@ package com.favouritedragon.arcaneessentials.client.render;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.util.math.MathHelper;
 import org.joml.Matrix4f;
 import org.joml.Vector4f;
 import org.lwjgl.opengl.GL11;
@@ -64,5 +65,56 @@ public class RenderUtils {
 		drawQuad(2, rtb, rtf, ltf, ltb, u1, v1, u2, v2); // +y
 		drawQuad(2, rtf, rbf, lbf, ltf, u1, v1, u2, v2); // -z
 		drawQuad(2, rtb, rbb, lbb, ltb, u1, v1, u2, v2); // +z
+	}
+
+	/**
+	 * Draws a sphere (using lat/long triangles) with the given parameters.
+	 * @param radius The radius of the sphere.
+	 * @param latStep The latitude step; smaller is smoother but increases performance cost.
+	 * @param longStep The longitude step; smaller is smoother but increases performance cost.
+	 * @param inside Whether to draw the outside or the inside of the sphere.
+	 * @param r The red component of the sphere colour.
+	 * @param g The green component of the sphere colour.
+	 * @param b The blue component of the sphere colour.
+	 * @param a The alpha component of the sphere colour.
+	 */
+	public static void drawSphere(float radius, float latStep, float longStep, boolean inside, float r, float g, float b, float a){
+
+		Tessellator tessellator = Tessellator.getInstance();
+		BufferBuilder buffer = tessellator.getBuffer();
+
+		buffer.begin(GL11.GL_TRIANGLE_STRIP, DefaultVertexFormats.POSITION_COLOR);
+
+		boolean goingUp = inside;
+
+		buffer.pos(0, goingUp ? -radius : radius, 0).color(r, g, b, a).endVertex(); // Start at the north pole
+
+		for(float longitude = -(float)Math.PI; longitude <= (float)Math.PI; longitude += longStep){
+
+			// Leave the poles out since they only have a single point per stack instead of two
+			for(float theta = (float)Math.PI/2 - latStep; theta >= -(float)Math.PI/2 + latStep; theta -= latStep){
+
+				float latitude = goingUp ? -theta : theta;
+
+				float hRadius = radius * MathHelper.cos(latitude);
+				float vy = radius * MathHelper.sin(latitude);
+				float vx = hRadius * MathHelper.sin(longitude);
+				float vz = hRadius * MathHelper.cos(longitude);
+
+				buffer.pos(vx, vy, vz).color(r, g, b, a).endVertex();
+
+				vx = hRadius * MathHelper.sin(longitude + longStep);
+				vz = hRadius * MathHelper.cos(longitude + longStep);
+
+				buffer.pos(vx, vy, vz).color(r, g, b, a).endVertex();
+			}
+
+			// The next pole
+			buffer.pos(0, goingUp ? radius : -radius, 0).color(r, g, b, a).endVertex();
+
+			goingUp = !goingUp;
+		}
+
+		tessellator.draw();
 	}
 }
