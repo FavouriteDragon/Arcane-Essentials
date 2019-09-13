c39d13d7946b85d5a1f4a68435088970df54474f
FavouriteDragon <ninjadragon008@icloud.com>
2019-09-12 17:25:31 -0700

    wayyy better spell icons!

--- src/main/java/com/favouritedragon/arcaneessentials/common/spell/storm/ThunderingChain.java
+++ src/main/java/com/favouritedragon/arcaneessentials/common/spell/storm/ThunderingChain.java
@@ -3,8 +3,11 @@ package com.favouritedragon.arcaneessentials.common.spell.storm;
 import com.favouritedragon.arcaneessentials.ArcaneEssentials;
 import com.favouritedragon.arcaneessentials.common.entity.EntityLightningSpawner;
 import electroblob.wizardry.registry.WizardryItems;
+import electroblob.wizardry.registry.WizardrySounds;
 import electroblob.wizardry.spell.Spell;
 import electroblob.wizardry.util.SpellModifiers;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumAction;
 import net.minecraft.util.EnumHand;
@@ -20,6 +23,15 @@ public class ThunderingChain extends Spell {
 
 	@Override
 	public boolean cast(World world, EntityPlayer caster, EnumHand hand, int ticksInUse, SpellModifiers modifiers) {
+		return cast(world, caster, modifiers);
+	}
+
+	@Override
+	public boolean cast(World world, EntityLiving caster, EnumHand hand, int ticksInUse, EntityLivingBase target, SpellModifiers modifiers) {
+		return cast(world, caster, modifiers);
+	}
+
+	private boolean cast(World world, EntityLivingBase caster, SpellModifiers modifiers) {
 		float damage = getProperty(DAMAGE).floatValue() * modifiers.get(SpellModifiers.POTENCY);
 		int lifetime = getProperty(DURATION).intValue() + (int) (10 * modifiers.get(WizardryItems.duration_upgrade));
 		int burnTime = getProperty(EFFECT_DURATION).intValue() + (int) (2 * modifiers.get(WizardryItems.duration_upgrade));
@@ -35,11 +47,12 @@ public class ThunderingChain extends Spell {
 		spawner.motionX = look.x;
 		spawner.motionY = 0;
 		spawner.motionZ = look.z;
+		caster.playSound(WizardrySounds.ENTITY_HAMMER_THROW, 1.0F + world.rand.nextFloat() / 10, 0.8F + world.rand.nextFloat() / 10);
+		caster.playSound(WizardrySounds.ENTITY_THUNDERBOLT_HIT, 1.0F + world.rand.nextFloat() / 10, 0.8F + world.rand.nextFloat() / 10);
 		if (!world.isRemote) {
 			return world.spawnEntity(spawner);
 		}
 		return false;
-
 	}
 
 	@Override
