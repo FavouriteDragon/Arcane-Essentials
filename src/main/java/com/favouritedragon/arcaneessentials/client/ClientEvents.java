d53919eeaa1968fddbba5872c8b3d07afa67bd9d
John <ninjadragon008@icloud.com>
2020-06-30 14:37:18 -0700

    wtf

--- src/main/java/com/favouritedragon/arcaneessentials/client/ClientEvents.java
+++ src/main/java/com/favouritedragon/arcaneessentials/client/ClientEvents.java
@@ -1,7 +1,9 @@
 package com.favouritedragon.arcaneessentials.client;
 
 import com.favouritedragon.arcaneessentials.ArcaneEssentials;
+import net.minecraft.client.renderer.block.model.ModelBakery;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.Mod;
@@ -15,21 +17,25 @@ import static com.favouritedragon.arcaneessentials.common.item.ArcaneItems.*;
 @Mod.EventBusSubscriber(value = Side.CLIENT, modid = ArcaneEssentials.MODID)
 public class ClientEvents {
 
-	@SubscribeEvent
-	public static void onModelRegistry(ModelRegistryEvent event) {
-		ModelLoader.setCustomModelResourceLocation(masterLightningSword, 0,
-				new ModelResourceLocation(Objects.requireNonNull(masterLightningSword.getRegistryName()), "inventory"));
-		ModelLoader.setCustomModelResourceLocation(masterSorcerySword, 0,
-				new ModelResourceLocation(Objects.requireNonNull(masterSorcerySword.getRegistryName()), "inventory"));
-		ModelLoader.setCustomModelResourceLocation(masterNatureSword, 0,
-				new ModelResourceLocation(Objects.requireNonNull(masterNatureSword.getRegistryName()), "inventory"));
-		ModelLoader.setCustomModelResourceLocation(masterFireSword, 0,
-				new ModelResourceLocation(Objects.requireNonNull(masterFireSword.getRegistryName()), "inventory"));
-		ModelLoader.setCustomModelResourceLocation(masterIceSword, 0,
-				new ModelResourceLocation(Objects.requireNonNull(masterIceSword.getRegistryName()), "inventory"));
-		ModelLoader.setCustomModelResourceLocation(masterHealingSword, 0,
-				new ModelResourceLocation(Objects.requireNonNull(masterHealingSword.getRegistryName()), "inventory"));
-		ModelLoader.setCustomModelResourceLocation(masterNecromancySword, 0,
-				new ModelResourceLocation(Objects.requireNonNull(masterNecromancySword.getRegistryName()), "inventory"));
-	}
+    @SubscribeEvent
+    public static void onModelRegistry(ModelRegistryEvent event) {
+        registerItemModel(masterLightningSword);
+        registerItemModel(masterIceSword);
+        registerItemModel(masterHealingSword);
+        registerItemModel(masterNatureSword);
+        registerItemModel(masterNecromancySword);
+        registerItemModel(masterFireSword);
+        registerItemModel(masterSorcerySword);
+    }
+
+    /**
+     * Registers an item model, using the item's registry name as the model name (this convention makes it easier to
+     * keep track of everything). Variant defaults to "normal". Registers the model for all metadata values.
+     */
+    private static void registerItemModel(Item item) {
+        // Changing the last parameter from null to "inventory" fixed the item/block model weirdness. No idea why!
+        ModelBakery.registerItemVariants(item, new ModelResourceLocation(item.getRegistryName(), "inventory"));
+        // Assigns the model for all metadata values
+        ModelLoader.setCustomMeshDefinition(item, s -> new ModelResourceLocation(item.getRegistryName(), "inventory"));
+    }
 }
