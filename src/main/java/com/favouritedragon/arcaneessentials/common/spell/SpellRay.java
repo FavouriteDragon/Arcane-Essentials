4b1e074b3551bea2974e2cd7cbebd0a9fb776110
John <ninjadragon008@icloud.com>
2021-02-08 11:44:00 -0800

    spell piercing works

--- src/main/java/com/favouritedragon/arcaneessentials/common/spell/SpellRay.java
+++ src/main/java/com/favouritedragon/arcaneessentials/common/spell/SpellRay.java
@@ -2,12 +2,16 @@ package com.favouritedragon.arcaneessentials.common.spell;
 
 import com.favouritedragon.arcaneessentials.ArcaneEssentials;
 import com.favouritedragon.arcaneessentials.common.util.ArcaneUtils;
-import electroblob.wizardry.util.*;
+import electroblob.wizardry.util.AllyDesignationSystem;
+import electroblob.wizardry.util.RayTracer;
+import electroblob.wizardry.util.SpellModifiers;
+import electroblob.wizardry.util.SpellProperties;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumAction;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.RayTraceResult;
@@ -26,7 +30,7 @@ public abstract class SpellRay extends electroblob.wizardry.spell.SpellRay imple
     public SpellRay(String name, EnumAction action, boolean isContinuous) {
         super(ArcaneEssentials.MODID, name, action, isContinuous);
         addProperties(EFFECT_RADIUS);
-        this.targetSelector = Entity::canBeAttackedWithItem;
+        this.targetSelector = entity -> !entity.getIsInvulnerable();
     }
 
 
@@ -42,6 +46,12 @@ public abstract class SpellRay extends electroblob.wizardry.spell.SpellRay imple
         this.targetSelector = targetSelector.and(entity -> !(entity == caster));
         playSound(world, caster);
         return super.cast(world, caster, hand, ticksInUse, target, modifiers);
+    }
+
+    @Override
+    public void finishCasting(World world, @Nullable EntityLivingBase caster, double x, double y, double z, @Nullable EnumFacing direction, int duration, SpellModifiers modifiers) {
+        super.finishCasting(world, caster, x, y, z, direction, duration, modifiers);
+        targetSelector = entity -> entity != caster;
     }
 
     public boolean isPiercing() {
@@ -103,10 +113,11 @@ public abstract class SpellRay extends electroblob.wizardry.spell.SpellRay imple
         // If flag is false, either the spell missed or the relevant entity/block hit method returned false
         if (!flag && !onMiss(world, caster, origin, direction, ticksInUse, modifiers)) return false;
 
-        //For piercing spells
-        else if (!flag && onMiss(world, caster, origin, direction, ticksInUse, modifiers) && isPiercing())
-            shootSpell(world, origin, direction, caster, ticksInUse, modifiers);
+            //For piercing spells
+        else if (flag && isPiercing() && rayTrace.typeOfHit == RayTraceResult.Type.ENTITY)
+            range = raytraceChain(world, origin, direction, caster, ticksInUse, modifiers);
 
+        targetSelector = entity -> entity != caster && AllyDesignationSystem.isValidTarget(caster, entity);
 
         // Particle spawning
         if (world.isRemote) {
@@ -114,5 +125,38 @@ public abstract class SpellRay extends electroblob.wizardry.spell.SpellRay imple
         }
 
         return true;
+    }
+
+    //Recursion to ensure all targets are properly hit
+    private double raytraceChain(World world, Vec3d origin, Vec3d direction, EntityLivingBase caster, int ticksInUse,
+                               SpellModifiers modifiers) {
+        double range = getRange(world, origin, direction, caster, ticksInUse, modifiers);
+        Vec3d endpoint = origin.add(direction.scale(range));
+        // Filter is negated because of weird raytrace shenanigans
+        RayTraceResult rayTrace = RayTracer.rayTrace(world, origin, endpoint, aimAssist, hitLiquids,
+                ignoreUncollidables, false, Entity.class, targetSelector.negate());
+
+        boolean flag = false;
+
+        if (rayTrace != null) {
+            // Doesn't matter which way round these are, they're mutually exclusive
+            if (rayTrace.typeOfHit == RayTraceResult.Type.ENTITY) {
+                // Do whatever the spell does when it hits an entity
+                targetSelector = targetSelector.and(entity -> AllyDesignationSystem.isValidTarget(caster, entity));
+                if (targetSelector.test(rayTrace.entityHit)) {
+                    flag = onEntityHit(world, rayTrace.entityHit, rayTrace.hitVec, caster, origin, ticksInUse, modifiers);
+                    targetSelector = targetSelector.and(entity -> entity != rayTrace.entityHit);
+                }
+                if (flag) range = origin.distanceTo(rayTrace.hitVec);
+
+            } else if (rayTrace.typeOfHit == RayTraceResult.Type.BLOCK) {
+                // Do whatever the spell does when it hits an block
+                flag = onBlockHit(world, rayTrace.getBlockPos(), rayTrace.sideHit, rayTrace.hitVec, caster, origin, ticksInUse, modifiers);
+                range = origin.distanceTo(rayTrace.hitVec);
+            }
+        }
+        if (flag && rayTrace.typeOfHit == RayTraceResult.Type.ENTITY)
+            raytraceChain(world, origin, direction, caster, ticksInUse, modifiers);
+        return range;
     }
 }
